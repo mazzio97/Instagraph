89658dc11d034b0f61ce8649f3ee1093d2cfd09a
Diego Mazzieri <diego.maz97@gmail.com>
2020-03-16 01:51:58 +0100

    Move buildGraph outside main and use PageRank on my Instagram graph

--- src/main/scala/com/instagraph/Instagraph.scala
+++ src/main/scala/com/instagraph/Instagraph.scala
@@ -2,15 +2,15 @@ package com.instagraph
 
 import java.nio.file.{Files, Paths}
 
-import com.instagraph.indices.PageRank._
-import com.instagraph.indices.BetweennessCentrality._
+import com.instagraph.indices.BetweennessCentrality.BetweennessCentrality
+import com.instagraph.indices.PageRank.PageRank
+
 import org.apache.spark.SparkConf
 import org.apache.spark.graphx.{Edge, Graph, VertexId}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object Instagraph {
-
   private val resourcesPath = "src/main/resources/"
 
   private val sparkConf  = new SparkConf()
@@ -23,37 +23,31 @@ object Instagraph {
     .getOrCreate()
 
   private def sparkContext = spark.sparkContext
-
   sparkContext.setLogLevel("ERROR")
 
+  def buildGraph(path: String): Graph[String, Unit] = {
+    val df = spark.read.json(path)
+    val users = df.columns
+    // Vertices of the graph are the users identified with an ID
+    val vertices: RDD[(VertexId, String)] = sparkContext.parallelize(
+      users.zipWithIndex.map { case (user, idx) => (idx.toLong, user) }
+    )
+    // Extract the ID of a vertex given the username
+    val correspondence: String => VertexId = user => vertices.filter(v => v._2 == user).first()._1
+    // Edges of the graph are following relations between any pair of vertices
+    val edges: RDD[Edge[Unit]] = sparkContext.parallelize(
+      df.collect()(0) // Extract first (and only) row for each column (user)
+        .getValuesMap[Seq[String]](users) // Map each vertex to its following
+        .mapValues[Seq[String]](_.filter(users.contains)) // Don't consider following outward vertices
+        .flatten { case (user, foll) => foll.map((user, _)) } // Convert map into pairs representing edges
+        .map( e => Edge(correspondence(e._1), correspondence(e._2), ()) ) // GraphX representation
+        .toSeq
+    )
+    Graph(vertices, edges)
+  }
+
   def main(args: Array[String]): Unit = {
-
-    def buildGraph(path: String): Graph[String, Unit] = {
-
-      val df = spark.read.json(path)
-
-      val users = df.columns
-
-      val vertices: RDD[(VertexId, String)] = sparkContext.parallelize(
-        users.zipWithIndex.map { case (user, idx) => (idx.toLong, user) }
-      )
-
-      // Extract the ID of a vertex given the username
-      val correspondence: String => VertexId = user => vertices.filter(v => v._2 == user).first()._1
-
-      val edges: RDD[Edge[Unit]] = sparkContext.parallelize(
-        df.collect()(0) // Extract first (and only) row for each column (user)
-          .getValuesMap[Seq[String]](users) // Map each vertex to its following
-          .mapValues[Seq[String]](_.filter(users.contains)) // Don't consider following outward vertices
-          .flatten { case (user, foll) => foll.map((user, _)) } // Convert map into pairs representing edges
-          .map( e => Edge(correspondence(e._1), correspondence(e._2), ()) ) // GraphX representation
-          .toSeq
-      )
-
-      Graph(vertices, edges)
-    }
-
-    // if the graph has already been stored it is loaded, otherwise it is built using the file "data.json"
+    // If the graph has already been stored it is loaded, otherwise it is built using the file "data.json"
     val graph: Graph[String, Unit] = if (Files.exists(Paths.get(resourcesPath + "graph"))) {
       val vertices = sparkContext.objectFile[(VertexId, String)](resourcesPath + "graph/vertices")
       val edges = sparkContext.objectFile[Edge[Unit]](resourcesPath + "graph/edges")
@@ -64,7 +58,15 @@ object Instagraph {
       graph.edges.saveAsObjectFile(resourcesPath + "graph/edges")
       graph
     }
-
-    // graph.pageRank.vertices.takeOrdered(graph.numVertices.toInt).foreach(println)
+    // Compute PageRank index on the graph
+    println("PageRank:")
+    graph.pageRank(0.001)
+      .vertices
+      .values
+      .sortBy(_._2, ascending = false)
+      .take(10)
+      .foreach(println)
+    // Compute Betweenness Centrality index on the graph
+    // graph.betweennessCentrality.vertices.takeOrdered(10).foreach(println)
   }
 }
