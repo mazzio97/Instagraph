941defbd6b79fe652f3119735010c3e371e6f01d
Diego Mazzieri <diego.maz97@gmail.com>
2020-03-17 17:57:24 +0100

    Move graph loading, building and saving operations in a dedicated class

--- src/main/scala/com/instagraph/Instagraph.scala
+++ src/main/scala/com/instagraph/Instagraph.scala
@@ -4,14 +4,14 @@ import java.nio.file.{Files, Paths}
 
 import com.instagraph.indices.BetweennessCentrality.BetweennessCentrality
 import com.instagraph.indices.PageRank.PageRank
-
+import com.instagraph.utils.GraphManager
 import org.apache.spark.SparkConf
-import org.apache.spark.graphx.{Edge, Graph, VertexId}
-import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx.Graph
 import org.apache.spark.sql.SparkSession
 
 object Instagraph {
-  private val resourcesPath = "src/main/resources/"
+  private val resourcesPath: String = "src/main/resources/"
+  private val graphPath = resourcesPath + "graph"
 
   private val sparkConf  = new SparkConf()
     .setMaster("local[*]")  // Master is running on a local node.
@@ -22,41 +22,15 @@ object Instagraph {
     .config(sparkConf)
     .getOrCreate()
 
-  private def sparkContext = spark.sparkContext
-  sparkContext.setLogLevel("ERROR")
-
-  def buildGraph(path: String): Graph[String, Unit] = {
-    val df = spark.read.json(path)
-    val users = df.columns
-    // Vertices of the graph are the users identified with an ID
-    val vertices: RDD[(VertexId, String)] = sparkContext.parallelize(
-      users.zipWithIndex.map { case (user, idx) => (idx.toLong, user) }
-    )
-    // Extract the ID of a vertex given the username
-    val correspondence: String => VertexId = user => vertices.filter(v => v._2 == user).first()._1
-    // Edges of the graph are following relations between any pair of vertices
-    val edges: RDD[Edge[Unit]] = sparkContext.parallelize(
-      df.collect()(0) // Extract first (and only) row for each column (user)
-        .getValuesMap[Seq[String]](users) // Map each vertex to its following
-        .mapValues[Seq[String]](_.filter(users.contains)) // Don't consider following outward vertices
-        .flatten { case (user, foll) => foll.map((user, _)) } // Convert map into pairs representing edges
-        .map( e => Edge(correspondence(e._1), correspondence(e._2), ()) ) // GraphX representation
-        .toSeq
-    )
-    Graph(vertices, edges)
-  }
-
   def main(args: Array[String]): Unit = {
+    val manager = new GraphManager[Int](spark)
     // If the graph has already been stored it is loaded, otherwise it is built using the file "data.json"
-    val graph: Graph[String, Unit] = if (Files.exists(Paths.get(resourcesPath + "graph"))) {
-      val vertices = sparkContext.objectFile[(VertexId, String)](resourcesPath + "graph/vertices")
-      val edges = sparkContext.objectFile[Edge[Unit]](resourcesPath + "graph/edges")
-      Graph(vertices, edges)
+    val graph: Graph[String, Int] = if (Files.exists(Paths.get(graphPath))) {
+      manager.load(graphPath)
     } else {
-      val graph = buildGraph(resourcesPath + "data.json")
-      graph.vertices.saveAsObjectFile(resourcesPath + "graph/vertices")
-      graph.edges.saveAsObjectFile(resourcesPath + "graph/edges")
-      graph
+      val g = manager.build(resourcesPath + "data.json", 1)
+      manager.save(g, graphPath)
+      g
     }
     // Compute PageRank index on the graph
     println("PageRank:")
---
+++ src/main/scala/com/instagraph/utils/GraphManager.scala
@@ -0,0 +1,44 @@
+package com.instagraph.utils
+
+import org.apache.spark.graphx.{Edge, Graph, VertexId}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+import scala.reflect.ClassTag
+
+class GraphManager[E: ClassTag](spark: SparkSession) {
+  private val sparkContext = spark.sparkContext
+  sparkContext.setLogLevel("ERROR")
+
+  def build(jsonPath: String, edgeValue: E): Graph[String, E] = {
+    val df = spark.read.json(jsonPath)
+    val users = df.columns
+    // Vertices of the graph are the users identified with an ID
+    val vertices: RDD[(VertexId, String)] = sparkContext.parallelize(
+      users.zipWithIndex.map { case (user, idx) => (idx.toLong, user) }
+    )
+    // Extract the ID of a vertex given the username
+    val correspondence: String => VertexId = user => vertices.filter(v => v._2 == user).first()._1
+    // Edges of the graph are following relations between any pair of vertices
+    val edges: RDD[Edge[E]] = sparkContext.parallelize(
+      df.collect()(0) // Extract first (and only) row for each column (user)
+        .getValuesMap[Seq[String]](users) // Map each vertex to its following
+        .mapValues[Seq[String]](_.filter(users.contains)) // Don't consider following outward vertices
+        .flatten { case (user, foll) => foll.map((user, _)) } // Convert map into pairs representing edges
+        .map(e => Edge(correspondence(e._1), correspondence(e._2), edgeValue)) // GraphX representation
+        .toSeq
+    )
+    Graph(vertices, edges)
+  }
+
+  def load(path: String): Graph[String, E] = {
+    val vertices = sparkContext.objectFile[(VertexId, String)](path + "/vertices")
+    val edges = sparkContext.objectFile[Edge[E]](path + "/edges")
+    Graph(vertices, edges)
+  }
+
+  def save(graph: Graph[String, E], path: String): Unit = {
+    graph.vertices.saveAsObjectFile(path + "/vertices")
+    graph.edges.saveAsObjectFile(path + "/edges")
+  }
+}
