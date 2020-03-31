63ddd662cceb43c60afff1c575b8df931898a130
Luca Giuliani <luca.giuliani10@studio.unibo.it>
2020-03-31 14:33:46 +0200

    Minor changes on BetwennessCentrality to handle initial message in a better way

--- src/main/scala/com/instagraph/indices/centrality/BetweennessCentrality.scala
+++ src/main/scala/com/instagraph/indices/centrality/BetweennessCentrality.scala
@@ -20,7 +20,7 @@ case class BetweennessCentrality[E: ClassTag](implicit numeric: Numeric[E]) exte
         val successorsMap: NormalizedSuccessorsMap = info.successors
           .filter { case (next, _) => next != destination }
           .mapValues { presences => presences.toDouble / totalShortestPaths }
-          .map(identity) // work-around to solve scala bug (https://github.com/scala/bug/issues/7005)
+          .map(identity) // https://github.com/scala/bug/issues/7005
         (destination, successorsMap)
       }.filter { case (_, info) => info.nonEmpty }
       VertexData(0.0, updatedShortestPaths, Map.empty)
@@ -29,7 +29,18 @@ case class BetweennessCentrality[E: ClassTag](implicit numeric: Numeric[E]) exte
     val vertexProgram: (VertexId, VertexData, ScoresMap) => VertexData =
       (_, vertexData, receivedScores) => {
         val newScore: Double = vertexData.score + receivedScores.values.sum
-        VertexData(newScore, vertexData.shortestPaths, receivedScores)
+        /*
+         * During the first iteration each vertex should get, as initial message, the map of each destination it can
+         * reach linked to the score of 1.0, namely a "complete" score that has not been split into or merged from
+         * different paths yet. Though, as the Pregel API allow to send a single message that is the same for each
+         * vertex, we decided to send an empty map and check that case (in no other iterations the vertex will receive
+         * an empty map, as a message will be sent if and only if the scores map contains at least one record) and
+         * and return the actual initial scores map instead.
+         */
+        val latestReceivedScores: ScoresMap =
+          if (receivedScores.nonEmpty) receivedScores
+          else vertexData.shortestPaths.mapValues(_ => 1.0).map(identity) // https://github.com/scala/bug/issues/7005
+        VertexData(newScore, vertexData.shortestPaths, latestReceivedScores)
       }
 
     val sendMessage: EdgeTriplet[VertexData, E] => Iterator[(VertexId, ScoresMap)] =
@@ -42,24 +53,16 @@ case class BetweennessCentrality[E: ClassTag](implicit numeric: Numeric[E]) exte
          * round those scores must be propagated forward until the destination, splitting the received value
          * respectively to the weight owned by each successor of the vertex through that specific destination.
          *
-         * The mapping function maps each destination to the score that must be assigned to the nextId vertex
-         * respectively to the path towards that specific destination (note that, as scores are propagated towards the
-         * destination, we do not have information about the origin of the path that we are considering)
-         *
-         * Finally, the mapping function is different whether the received scores map is empty or not.
-         * This is useful during the first iteration, as the vertex will receive an empty map for scores from the
-         * Pregel API in order to start sending messages. In this specific case, indeed, we are sure that the vertices
-         * we are considering are the origin of the paths, hence we will know that the score value is "initialized"
-         * in this specific vertex and will have the value of 1.0 (namely a "complete" score that has not been split
-         * into or merged from different paths yet)
+         * The mapping function to get the score for each destination maps each destination to the score that must be
+         * assigned to the nextId vertex respectively to the path towards that specific destination (note that, as
+         * scores are propagated towards the destination, we do not have information about the origin of the path that
+         * we are considering)
          */
-        val mappingFunction: (VertexId, NormalizedSuccessorsMap) => Double =
-          if (receivedScores.isEmpty) (_, successorsMap) => successorsMap.getOrElse(nextId, 0.0)
-          else (destinationId, successorsMap) => receivedScores.get(destinationId)
+        val scoresMap: ScoresMap = shortestPaths.map { case (destinationId, successorsMap) =>
+          val score: Double = receivedScores.get(destinationId)
             .map(score => score * successorsMap.getOrElse(nextId, 0.0))
             .getOrElse(0.0)
-        val scoresMap: ScoresMap = shortestPaths.map { case (destinationId, successorsMap) =>
-          (destinationId, mappingFunction(destinationId, successorsMap))
+          (destinationId, score)
         }.filter { case (_, score) => score != 0 }
         if (scoresMap.isEmpty) Iterator.empty else Iterator((nextId, scoresMap))
       }
