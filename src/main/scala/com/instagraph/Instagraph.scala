556940555667149e897966054bf9a49f580ce933
Luca Giuliani <luca.giuliani10@studio.unibo.it>
2020-04-08 17:10:43 +0200

    Refactor graph build from json files, no need to store graph anymore

--- src/main/scala/com/instagraph/Instagraph.scala
+++ src/main/scala/com/instagraph/Instagraph.scala
@@ -1,39 +1,41 @@
 package com.instagraph
 
-import java.nio.file.{Files, Paths}
-
-import com.instagraph.utils.GraphManager
-import org.apache.spark.SparkConf
-import org.apache.spark.graphx.Graph
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.graphx.{Edge, Graph, VertexId}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
+import scala.collection.mutable
 import scala.reflect.ClassTag
 
-object Instagraph {
-  private val graphsPath = System.getProperty("user.home") + "/.instagraph/"
+case class Instagraph(sparkConf: SparkConf = new SparkConf().setAppName("Instagraph")) {
+  private val spark: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+  private val sparkContext: SparkContext = spark.sparkContext
+  sparkContext.setLogLevel("ERROR")
 
-  private val sparkConf = new SparkConf().setAppName("Instagraph")
+  def followersGraph[E: ClassTag](edgeWeight: Set[String] => E, jsonFiles: String*): Graph[String, E] = {
+    // RDD containing the future nodes of the graph (both id and value, namely the username)
+    // associated to their respective list of following accounts
+    val usersRDD: RDD[(Long, String, Set[String])] = sparkContext.parallelize(
+      jsonFiles.map(spark.read.json(_))
+        .flatMap(df => df.head.getValuesMap[mutable.WrappedArray[String]](df.columns))
+        .groupBy { case (username, _) => username }
+        .mapValues(_.flatMap { case (_, following) => following }.toSet)
+        .toSeq
+    ).zipWithIndex().map { case ((username, following), id) => (id, username, following) }
 
-  private val spark = SparkSession
-    .builder()
-    .config(sparkConf)
-    .getOrCreate()
+    // a local map of the users indexed by value useful for a faster computation of the relationships (the edges)
+    val usersMap: Map[String, Long] = usersRDD.map { case (id, username, _) => (username, id) }.collectAsMap().toMap
 
-  def followersGraph[E: ClassTag](edgeWeight: Seq[String] => E, jsonFiles: String*): Graph[String, E] = {
-    val manager = new GraphManager[E](spark)
-    val graphName = jsonFiles.map(path => path.split("/").last)
-      .map(name => name.takeWhile(c => c != '.'))
-      .sorted
-      .mkString("+")
-    val graphPath = graphsPath + graphName
-    // If the graph has already been stored it is loaded, otherwise it is built and saved for future loadings
-    if (Files.exists(Paths.get(graphPath))) {
-      manager.load(graphPath)
-    } else {
-      val g = manager.build(edgeWeight, jsonFiles:_*)
-      manager.save(g, graphPath)
-      g
+    // the RDD of edges is created from the RDD of users and their respective following
+    val edges: RDD[Edge[E]] = usersRDD.flatMap { case (id, _, following) =>
+      val weight: E = edgeWeight(following)
+      following.map(usersMap.get)
+        .filter(followingOption => followingOption.isDefined)
+        .map(followingId => Edge(id, followingId.get, weight))
     }
+
+    Graph(usersRDD.map { case(id, username, _) => (id, username) }, edges)
   }
 
   def unweightedFollowersGraph(jsonFiles: String*): Graph[String, Int] = followersGraph[Int](_ => 1, jsonFiles:_*)
